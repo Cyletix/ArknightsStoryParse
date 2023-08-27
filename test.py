feb413265025d5a9b5d6d83e8a61c50270dd8ed8
Cyletix <1016120209@qq.com>
2023-08-27 16:49:29 +0900

    vault backup: 2023-08-27 16:49:29

---
+++ test.py
@@ -0,0 +1,53 @@
+# 解析文本正则测试
+
+import re
+
+juqing = []
+juqing.append(r"""[name="能天使"] 牛逼""")
+juqing.append(r"""[name="拜松&能天使"]   又来？""")
+
+# 编译正则表达式模式
+re_mingzi = re.compile(r'\[name="(.*?)"\]')
+re_duihua = re.compile(']\s*(.*)')
+re_huifu = re.compile('"(.*)",')
+re_xuanxiang = re.compile('"(.*)"')
+
+jiexijuqing=[]
+
+for i in juqing:
+    # 第一种类型 对话
+    if i[:5] == "[Name" or i[:5] =="[name":
+        mingzi=re_mingzi.findall(i)
+        duihua=re_duihua.findall(i)
+        
+        # print(mingzi[0]+": "+duihua[0])
+        jiexijuqing.append('[['+mingzi[0]+"]]: "+duihua[0])
+    
+    # 第二种类型 无人物文字
+    if i[:5] =="[Dial" or i[:5] =="[dial" :
+        # print("")
+        jiexijuqing.append("")
+    
+    # 第三种类型 选择支
+    if i[:5] =="[Deci" or i[:5] =="[deci" :
+        huifulist = re_huifu.findall(i)[0].split(";")
+        # print("\n下面是可选择的回复：")
+        jiexijuqing.append("\n下面是可选择的回复：")
+        j = 1
+        for huifu in huifulist:
+            # print("    选项{}：".format(j)+huifu)
+            jiexijuqing.append("    选项{}：".format(j)+huifu)
+            j+=1
+    if i[:5] =="[Pred" or i[:5] =="[pred" :
+        xuanxianglist = re_xuanxiang.findall(i)
+        
+        # print("\n下面是回复选项{}的剧情：".format(xuanxianglist))
+        jiexijuqing.append("\n下面是回复选项{}的剧情：".format(xuanxianglist))
+    
+    # 遇到文字直接输出
+    if i[:1] !="[":
+        # print(i)
+        jiexijuqing.append(i)
+
+
+print(jiexijuqing)
--- 解包剧情阅读器.py
+++ 解包剧情阅读器.py
@@ -13,14 +13,14 @@ def parse_arknights_story(file_path_name,save_path_name):
 
     with open(file_path_name, 'r',encoding='utf-8') as f:
         juqing = f.readlines()
-
+    # 编译正则表达式模式
     re_mingzi = re.compile('name= ?"(.*)"')
     re_duihua = re.compile(']\s*(.*)')
     re_huifu = re.compile('"(.*)",')
     re_xuanxiang = re.compile('"(.*)"')
 
     jiexijuqing=[]
-
+    # 按行解析
     for i in juqing:
         # 第一种类型 对话
         if i[:5] == "[Name" or i[:5] =="[name":
@@ -31,12 +31,12 @@ def parse_arknights_story(file_path_name,save_path_name):
             jiexijuqing.append('[['+mingzi[0]+"]]: "+duihua[0])
         
         # 第二种类型 无人物文字
-        if i[:5] =="[Dial" or i[:5] =="[dial" :
+        elif i[:5] =="[Dial" or i[:5] =="[dial" :
             # print("")
             jiexijuqing.append("")
         
         # 第三种类型 选择支
-        if i[:5] =="[Deci" or i[:5] =="[deci" :
+        elif i[:5] =="[Deci" or i[:5] =="[deci" :
             huifulist = re_huifu.findall(i)[0].split(";")
             # print("\n下面是可选择的回复：")
             jiexijuqing.append("\n下面是可选择的回复：")
@@ -45,14 +45,14 @@ def parse_arknights_story(file_path_name,save_path_name):
                 # print("    选项{}：".format(j)+huifu)
                 jiexijuqing.append("    选项{}：".format(j)+huifu)
                 j+=1
-        if i[:5] =="[Pred" or i[:5] =="[pred" :
+        elif i[:5] =="[Pred" or i[:5] =="[pred" :
             xuanxianglist = re_xuanxiang.findall(i)
             
             # print("\n下面是回复选项{}的剧情：".format(xuanxianglist))
             jiexijuqing.append("\n下面是回复选项{}的剧情：".format(xuanxianglist))
         
         # 遇到文字直接输出
-        if i[:1] !="[":
+        elif i[:1] !="[":
             # print(i)
             jiexijuqing.append(i)
 
@@ -65,19 +65,19 @@ def parse_arknights_story(file_path_name,save_path_name):
 
 if __name__=='__main__':
     path=r"ArknightsGameData"
-    story_language = input('请选择story语言(1.Chinese; 2.Japanese; 3.English;):')
-    if story_language == '1':
-        path += r'\zh_CN\story'
-    elif story_language == '2':
-        path += r'\ja_JP\story'
-    elif story_language == '3':
-        path += r'\en_US\story'
-    else:
-        print("无效的选择")
-        exit(1)
+    # story_language = input('请选择story语言(1.Chinese; 2.Japanese; 3.English;):')
+    # if story_language == '1':
+    #     path += r'\zh_CN\story'
+    # elif story_language == '2':
+    #     path += r'\ja_JP\story'
+    # elif story_language == '3':
+    #     path += r'\en_US\story'
+    # else:
+    #     print("无效的选择")
+    #     exit(1)
 
 
-    path=r"ArknightsGameData\zh_CN\story"# 目前仅支持chinese, 多语言会巨卡
+    path=r"ArknightsGameData\zh_CN\gamedata\story"# 目前仅支持chinese, 多语言会巨卡
 
 
     file_path_list=walkFile(path)
