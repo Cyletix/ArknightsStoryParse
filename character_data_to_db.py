e7f72286ef50741aec25d61f976b2f8acb5512dd
Cyletix <1016120209@qq.com>
2025-08-16 23:04:33 +0800

    更新插件配置，启用切换器功能；调整图形缩放比例；修改工作区布局，更新最近打开的文件；优化数据库写入脚本，添加角色数据表格文件。

--- character_data_to_db.py
+++ character_data_to_db.py
@@ -1,49 +1,55 @@
-'''
+"""
 Description: 写入数据库,新流程不再从'https://prts.wiki/w/'的html文件爬取,改用ArknightsGameData项目解包文件获取
 Author: Cyletix
 Date: 2023-08-06 19:29:59
 LastEditTime: 2023-08-07 21:48:47
-'''
+"""
+
 import json
 import pandas as pd
 
 
-character_table_path = 'ArknightsGameData\zh_CN\excel\character_table.json'
+character_table_path = r"ArknightsGameData\zh_CN\gamedata\excel\character_table.json"
 
 # 使用 with 语句打开json文件，确保在操作完成后自动关闭文件
-with open(character_table_path, 'r', encoding='utf-8') as file:
+with open(character_table_path, "r", encoding="utf-8") as file:
     json_data = json.load(file)
 
-#查看char_285_medic2的所有属性
+# 查看char_285_medic2的所有属性
 # for key in json_data["char_285_medic2"].keys():
 #     print(key)
 
 
 def get_char_keys_list():
     # 获取干员char_key列表
-    char_keys_list = [{"char_key": char_key} for char_key in json_data if char_key.startswith("char_")]
+    char_keys_list = [
+        {"char_key": char_key} for char_key in json_data if char_key.startswith("char_")
+    ]
     return char_keys_list
+
 
 def get_name_list():
     # 获取chinese name列表
-    name_list = [char_data["name"] for char_key, char_data in json_data.items() if char_key.startswith("char_")]
+    name_list = [
+        char_data["name"]
+        for char_key, char_data in json_data.items()
+        if char_key.startswith("char_")
+    ]
     return name_list
 
 
-if __name__ == '__main__':
-    char_keys_list=get_char_keys_list()
-    name_list=get_name_list()
+if __name__ == "__main__":
+    char_keys_list = get_char_keys_list()
+    name_list = get_name_list()
 
     # 查找未插入数据库的干员
-    op_not_in_db = search_sql(char_keys_list)
+    # op_not_in_db = search_sql(char_keys_list)
 
+    # from mypgsql import search_sql, pg_query
+    # import mypgsql2
 
-    from mypgsql import search_sql, pg_query
-    import mypgsql2
     # 插入到数据库
-    mypgsql2.insert_char_keys(op_not_in_db)
-
-
+    # mypgsql2.insert_char_keys(op_not_in_db)
 
     # 创建一个空的列表，用于存储每个角色的属性
     character_data_list = []
@@ -60,14 +66,11 @@ if __name__ == '__main__':
             }
             character_data_list.append(character_data)
 
+    # if input("是否导入数据库\n") == "Y":
+    #     # 插入函数,测试中...
+    #     mypgsql2.insert_char_keys(character_data_list)
 
-    if input('是否导入数据库\n')=='Y':
-        # 插入函数,测试中...
-        mypgsql2.insert_char_keys(character_data_list)
-
-
-
-    if input('是否输出excel\n')=='Y':
+    if input("是否输出excel\n") == "Y":
         # 将列表转换为 pandas 的 DataFrame
         df = pd.DataFrame(character_data_list)
 
@@ -78,22 +81,12 @@ if __name__ == '__main__':
         print(f"已将数据保存到 {output_file_path} 文件中。")
 
 
-
-
-
-
-
-
-
-
-
-
 # ####################################################
 # # 遍历所有的 "char_285_medic" 数据
 # for char_key, char_data in json_data.items():
 #     # 获取 "data" 属性
 #     data_list = char_data.get("phases", [])[0].get("attributesKeyFrames", [])
-    
+
 #     # 遍历 "data" 属性中的列表
 #     for data in data_list:
 #         level = data.get("level")
